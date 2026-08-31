5afc9728e6b67ea1af28a07226efa860ebb9bc18
pradarsha-kumar-nayak <pradarshanayak77@gmail.com>
2026-09-01 00:19:14 +0530

    Time: 121 ms (5.61%) | Memory: 48.1 MB (15.17%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,82 @@
+class Solution {
+    class hlp{
+        int dif;
+        int row;
+        int col;
+
+        public hlp(int dif,int row,int col){
+            this.dif=dif;
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public int minimumEffortPath(int[][] heights) {
+        int n=heights.length;
+        int m=heights[0].length;
+        int diff[][]=new int[n][m];
+        boolean vis[][]=new boolean[n][m];
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                diff[i][j]=Integer.MAX_VALUE;
+            }
+        }
+         diff[0][0]=0;
+        PriorityQueue<hlp>pq=new PriorityQueue<>((a,b)->a.dif-b.dif);
+        
+        pq.add(new hlp(0,0,0));
+
+        while(!pq.isEmpty()){
+           hlp cur=pq.remove();
+           int prevdif=cur.dif;
+           int i=cur.row;
+           int j=cur.col;
+           int prevval=heights[i][j];
+
+           if(vis[i][j]==true)continue;
+
+           vis[i][j]=true;
+          
+        
+        //right
+           if(j+1 < m){
+            int curval=heights[i][j+1];
+            int curdif=Math.abs(prevval-curval);
+            int maxdif=Math.max(prevdif,curdif);
+            diff[i][j+1]=Math.min(diff[i][j+1],maxdif);
+            pq.add(new hlp(maxdif,i,j+1));
+           }
+
+        //left
+           if(j-1 >=0){
+            int curval=heights[i][j-1];
+            int curdif=Math.abs(prevval-curval);
+            int maxdif=Math.max(prevdif,curdif);
+            diff[i][j-1]=Math.min(diff[i][j-1],maxdif);
+             pq.add(new hlp(maxdif,i,j-1));
+           }
+
+        //down
+           if(i+1 <n){
+            int curval=heights[i+1][j];
+            int curdif=Math.abs(prevval-curval);
+            int maxdif=Math.max(prevdif,curdif);
+            diff[i+1][j]=Math.min(diff[i+1][j],maxdif);
+             pq.add(new hlp(maxdif,i+1,j));
+           }
+
+        //top
+           if(i-1 >=0){
+            int curval=heights[i-1][j];
+            int curdif=Math.abs(prevval-curval);
+            int maxdif=Math.max(prevdif,curdif);
+            diff[i-1][j]=Math.min(diff[i-1][j],maxdif);
+             pq.add(new hlp(maxdif,i-1,j));
+           }
+
+        }
+
+        return diff[n-1][m-1];
+
+    }
+}
