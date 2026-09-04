3a4ad10901ee608fc1f0fd1f0c374030dba018be
pradarsha-kumar-nayak <pradarshanayak77@gmail.com>
2026-09-04 11:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (64.82%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -27,15 +27,7 @@ class Solution {
         m=grid[0].length;
         int dp[][]=new int[n][m];
 
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         dp[i][j]=-1;
-        //     }
-        // }
-
-        if(n==1 && m==1 && grid[0][0]==1){
-            return 0;
-        }
+       
         
          dp[0][0]=(grid[0][0]==1)?0:1;
 
