babbd83b229317d3eb33bed599e4d8b16cda0610
pradarsha-kumar-nayak <pradarshanayak77@gmail.com>
2026-09-21 09:41:08 +0530

    Time: 7 ms (0.00%) | Memory: 46.9 MB (0.00%) - LeetSync

---
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int binarysch(int st,int end,int tar,int[][] intervals){
+        int fstst=st;
+        while(st <= end){
+
+            int mid=(st+end)/2;;
+
+            if(intervals[mid][0] <= tar){
+               st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+
+        return st-fstst;
+    }
+    public int countIntersectingIntervals(int[][] intervals) {
+        int n=intervals.length;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int intervalcnt[]=new int[n];
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+
+            ans+=binarysch(i+1,n-1,intervals[i][1],intervals);
+        }
+
+        return ans;
+    }
+}
