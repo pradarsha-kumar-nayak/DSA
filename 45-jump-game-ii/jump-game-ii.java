05d8c050d721c2063283990f2a678046581eb630
pradarsha-kumar-nayak <pradarshanayak77@gmail.com>
2026-09-22 12:27:01 +0530

    Time: 52 ms (8.99%) | Memory: 46.9 MB (93.45%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,30 +1,48 @@
 class Solution {
-    int n;
-    public int helper(int nums[],int idx,int dp[]){
-        if(idx >=n-1){
-            return 0;
-        }
+    // int n;
+    // public int helper(int nums[],int idx,int dp[]){
+    //     if(idx >=n-1){
+    //         return 0;
+    //     }
         
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int min=Integer.MAX_VALUE;
+    //     if(dp[idx] != -1){
+    //         return dp[idx];
+    //     }
+    //     int min=Integer.MAX_VALUE;
         
-        for(int i=1;i<=nums[idx];i++){
-            int result=helper(nums,idx+i,dp);
+    //     for(int i=1;i<=nums[idx];i++){
+    //         int result=helper(nums,idx+i,dp);
 
-            if(result !=Integer.MAX_VALUE){
-               min=Math.min(min,1+result);
-            }
+    //         if(result !=Integer.MAX_VALUE){
+    //            min=Math.min(min,1+result);
+    //         }
             
-        }
+    //     }
 
-        return dp[idx]=min;
-    }
+    //     return dp[idx]=min;
+    // }
     public int jump(int[] nums) {
-        n=nums.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,-1);
-      return  helper(nums,0,dp);
+    //     n=nums.length;
+    //     int dp[]=new int[n];
+    //     Arrays.fill(dp,-1);
+    //   return  helper(nums,0,dp);
+         int n=nums.length;
+         int dp[]=new int[n];
+         Arrays.fill(dp,Integer.MAX_VALUE);
+         dp[n-1]=0;
+         
+         for(int idx=n-2;idx>=0;idx--){
+
+            for(int i=1;i<=nums[idx] && idx+i <n;i++){
+                
+                if(dp[idx+i] !=Integer.MAX_VALUE){
+                   dp[idx]=Math.min(dp[idx],1+dp[idx+i]);
+                }
+                
+            }
+         }
+
+         return dp[0];
+
     }
 }
